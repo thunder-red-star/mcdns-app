c863ae3fa422e5c0cb2a3988d97f1be24715f36c
thundering redstar <wvongphanith30@gmail.com>
2022-12-15 15:52:42 +0000

    e

---
+++ src/utils/serverStart.js
@@ -0,0 +1,12 @@
+const { exec } = require('child_process');
+
+module.exports = function serverStart(dir) {
+	// Start the Minecraft server
+	exec(`bash ${dir}/launch.sh`, (err, stdout, stderr) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		console.log(stdout);
+	}
+}
