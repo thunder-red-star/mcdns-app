fea87045e5d93ec011bbd9819d51656cdb915e34
thundering redstar <wvongphanith30@gmail.com>
2022-12-16 19:36:27 +0000

    e

--- src/utils/servers/index.js
+++ src/utils/servers/index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const motdParser = require("../parse/motd");
 const propertiesParser = require("../parse/properties");
 const minecraftServerUtil = require("minecraft-server-util");
+const Axios = require("axios");
 
 module.exports = {
   load: (dir) => {
@@ -71,4 +72,63 @@ module.exports = {
       return false;
     }
   },
+
+  create: async (serverName, serverPort, serverType, serverVersion, ramAlloc) => {
+    // Types: vanilla, paper
+    // Create directory: global.config.minecraft.serversDirectory + / + serverName
+    const serverDirectory = path.join(global.config.minecraft.serversDirectory, serverName);
+    // Create the directory
+    fs.mkdirSync(serverDirectory);
+    // Create server.properties by loading server.properties from the template directory and replacing the values
+    const templateProperties = fs.readFileSync('server.properties', 'utf8');
+    const parsedProperties = propertiesParser.parse(templateProperties);
+    parsedProperties['server-port'] = serverPort;
+    parsedProperties['level-name'] = serverName;
+    parsedProperties['rcon.port'] = serverPort + 10000;
+    parsedProperties['rcon.password'] = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    const newProperties = propertiesParser.dump(parsedProperties);
+    fs.writeFileSync(path.join(serverDirectory, 'server.properties'), newProperties);
+    // Create eula.txt
+    fs.writeFileSync(path.join(serverDirectory, 'eula.txt'), 'eula=true');
+    // Copy launch.sh to the server directory
+    fs.copyFileSync('launch.sh', path.join(serverDirectory, 'launch.sh'));
+    // Write a file called ramsize with the ram allocation
+    fs.writeFileSync(path.join(serverDirectory, 'ramsize'), ramAlloc);
+    
+    // Now this is the hard part: depending on the server type, download the server jar.
+    if (serverType === 'vanilla') {
+      // Download the server jar
+      const response = await Axios.get(`https://launchermeta.mojang.com/mc/game/version_manifest.json`);
+      const versions = response.data.versions;
+      const version = versions.find(v => v.id === serverVersion);
+      const serverUrl = version.url;
+      const serverResponse = await Axios.get(serverUrl);
+      const serverDownloadUrl = serverResponse.data.downloads.server.url;
+      const serverJar = await Axios.get(serverDownloadUrl, { responseType: 'stream' });
+      serverJar.data.pipe(fs.createWriteStream(path.join(serverDirectory, 'server.jar')));
+      // Wait for the download to finish
+      await new Promise(resolve => {
+	serverJar.data.on('end', () => {
+	  resolve();
+	});
+      }
+    } else if (serverType === 'paper') {
+      // Download the server jar
+      const builds = await Axios.get(`https://api.papermc.io/v2/projects/paper/versions/${serverVersion}/builds`);
+      // Choose the latest build
+      const build = builds.data[builds.data.length - 1];
+      const serverDownloadUrl = `https://papermc.io/api/v2/projects/paper/versions/${serverVersion}/builds/${build}/downloads/paper-${serverVersion}-${build}.jar`;
+      const serverJar = await Axios.get(serverDownloadUrl, { responseType: 'stream' });
+      serverJar.data.pipe(fs.createWriteStream(path.join(serverDirectory, 'server.jar')));
+      // Wait for the download to finish
+      await new Promise(resolve => {
+        serverJar.data.on('end', () => {
+	  resolve();
+	});
+      }
+    } else {
+      // Unsupported server type
+      global.logger.error(`Unsupported server type: ${serverType}`);
+    }
+  }
 };
